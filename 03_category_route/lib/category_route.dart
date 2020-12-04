92ff82bc98e45a6d21b4c36a7df79090e272c6ee
David Miguel Lozano <me@davidmiguel.com>
2020-12-04 09:03:34 +0100

    03_category_route

---
+++ 03_category_route/lib/category_route.dart
@@ -0,0 +1,67 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:task_03_category_route/category.dart';
+
+final _backgroundColor = Colors.green[100];
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute();
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final listView = ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: _categoryNames.length,
+      itemBuilder: (context, index) => Category(
+        name: _categoryNames[index],
+        color: _baseColors[index],
+        iconLocation: Icons.cake,
+      ),
+    );
+
+    final appBar = AppBar(
+      title: const Text(
+        'Unit Converter',
+        style: TextStyle(fontSize: 30),
+      ),
+      backgroundColor: _backgroundColor,
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
